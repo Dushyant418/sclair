8c3928a5e6b93e5df07cd7f801ab3de3fcc941d5
Dushyant418 <dushyantlochab335@gmail.com>
2024-06-25 10:55:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Fully from './components/Fully';
+import Hero from './components/Hero'
+import Questions from './components/Questions';
+import Roadmap from './components/Roadmap';
+import Smart from './components/Smart';
+import Socials from './components/Socials';
+import Tokenomics from './components/Tokenomics';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <About />
+      <Smart />
+      <Tokenomics />
+      <Fully />
+      <Roadmap />
+      <Questions />
+      <Socials />
+      <Footer />
+    </>
   );
 }
 
